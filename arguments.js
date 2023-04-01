fd1931bfc47829d3072d960974a6d3bfdc9ad305
Oksana Petryshyn <opetryshyn2@gmail.com>
2023-04-01 11:25:58 +0300

    fix

--- arguments.js
+++ arguments.js
@@ -2,6 +2,8 @@
 // яка приймає произвольну кількість аргументів і повертає їх
 // среднє значення.Всі аругменти будуть тільки числами.
 
+// function calculateAverage() {}
+
 // console.log(calculateAverage(1, 2, 3, 4)); // 2.5
 // console.log(calculateAverage(14, 8, 2)); // 8
 // console.log(calculateAverage(27, 43, 2, 8, 36)); // 23.2
